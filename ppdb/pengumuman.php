29955eec0d905357bd66052d73c7b272ac22434d
rhn.id <drehan566@gmail.com>
2025-06-29 17:35:14 +0700

    *

--- ppdb/pengumuman.php
+++ ppdb/pengumuman.php
@@ -9,4 +9,27 @@
             </div>
         </div>
         <!-- Header End -->
+            <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="card">
+                    <div class="card-header text-center py-3">
+                        Pengumuman PPDB SMK CANDA BHIRAWA PARE
+                    </div>
+                    <div class="card-body">
+                        <ol class="list-group list-group-numbered">
+                            <li class="list-group-item d-flex align-items-start">
+                                <div class="ms-2 me-auto">
+                                    belum ada pengumuman
+                                </div>
+                            </li>
+                            
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<br>
+<br>
 <?php  include('./layout/footer.php') ?>
